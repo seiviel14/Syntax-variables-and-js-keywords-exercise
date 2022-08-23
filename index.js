15f4f671a1e7f861898e9291cd7ce1c83ce76854
seiviel14 <senriquevelasquez@gmail.com>
2022-08-23 19:16:04 +0200

    Exercise completed

---
+++ index.js
@@ -0,0 +1,15 @@
+const my_name = "Samuel";
+const my_age = 22;
+const am_developer = true;
+const date_of_birth = new Date('March 14, 2000');
+const fav_book = {
+    Title: 'The hero of the ages',
+    Author: 'Brandon Sanderson',
+    Publication_date: 'October 14, 2008',
+    Url: 'https://www.amazon.es/Mistborn-Hero-Ages-Brandon-Sanderson/dp/0765356147' 
+};
+
+
+
+const exercise_list = [my_name,my_age,am_developer,date_of_birth,fav_book];
+console.log(exercise_list);
